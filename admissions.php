018bb6b8623c10681dc291f34e140048a2bd7e0e
vuxuanson0810 <you@example.com>
2022-06-14 22:42:53 +0700

    bo sung

---
+++ admissions.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tuyển sinh</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/admissions.css">
+</head>
+<body>
+    <div class="container-full">
+        <div class="header">
+            <div class="globalnav">
+                <div class="container">
+                    <div class="header_top">
+                        <div class="label">
+                            <a href="" class="ul_link_white">Trường đại học Thủy Lợi - TLU</a>
+                        </div>
+                        <ul class="list_top">
+                            <li>(+) | </li>
+                            <li>
+                                <div id="dnn_dnnLogin_loginGroup" class="loginGroup">
+                                    <a id="dnn_dnnLogin_enhancedLoginLink" title="Login" class="LoginLink" rel="nofollow" onclick="return dnnModal.show('http://cse.tlu.edu.vn/Login?returnurl=/&amp;popUp=true',/*showReturn*/true,300,650,true,'')" href="http://cse.tlu.edu.vn/Login?returnurl=%2f">Login</a>
+                                </div>
+                            </li>
+                            <li>|</li>
+                            <li>
+                            </li>
+                            <li>|</li>
+                            <li class="language">
+                                <span class="language-text"> Ngôn ngữ: &nbsp;&nbsp;&nbsp;&nbsp;</span>
+                                <a href="#" title="Tiếng Việt" class="ui-link-white lang-vi"><img src="/assets/images/icons/icon-lang-vi.png" alt="Tiếng Việt"></a>
+                                <a href="#" title="Tiếng Anh" class="ui-link-white lang-en"><img src="http://cse.tlu.edu.vn/cse/assets/images/icons/icon-lang-en.png" alt="Tiếng Anh"></a>
+                            </li>
+                            <form class="d-flex" role="search">
+                                <input class="header_seach" type="search" placeholder="Tìm kiếm" aria-label="Search">
+                                <input type="submit" name="dnn$ctr681$ViewCMSSearch$ctl00$btnSearch" value="Tìm" id="dnn_ctr681_ViewCMSSearch_ctl00_btnSearch" class="searchbutton">
+                            </form>
+                        </ul>
+                        <div class="container_search">
+        
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="navtop">
+                <div class="container" style="margin: 0 482px;">
+                    <div class="header_logo">
+                        <a href="#">
+                            <img src="/assets/images/icons/logo.png" alt="">
+                        </a>
+                    </div>
+
+                    <div class="header_menu">
+                        <ul class="header_menu_list">
+                            <li>
+                                <a href="">Trang chủ</a>
+                            </li>
+                            <li>
+                                <a href="">Giới thiệu</a>
+                                <ul class="menu_list_child">
+                                    <li><a href="">Logo khoa CNTT</a></li>
+                                    <li><a href="">Lời chào mừng</a></li>
+                                    <li><a href="">Tổ chức</a></li>
+                                    <li><a href="">Hợp tác liên kết</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="">Nghiên cứu khoa học</a>
+                                <ul class="menu_list_child">
+                                    <li><a href="">Các đề tài dự án</a></li>
+                                    <li><a href="">Thông tin seminar</a></li>
+                                    <li><a href="">Công trình công bố</a></li>
+                                    <li><a href="">Các phòng thí nghiệm</a></li>
+                                    <li><a href="">ông trình công bố</a></li>
+                                    <li><a href="">Các phòng thí nghiệm</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="">Đào tạo</a>
+                                <ul class="menu_list_child">
+                                    <li><a href="">Đào tạo đại học</a></li>
+                                    <li><a href=""></a>Đào tạo sau đại học</li>
+                                    <li><a href=""></a>Mô tả chương trình đào tạo</li>
+                                    <li><a href=""></a>Định hướng nghề</li>
+                                    <li><a href=""></a>Chuẩn đầu ra</li>
+                                    <li><a href=""></a>Đề cương môn học</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="">Bộ môn - Trung tâm</a>
+                                <ul class="menu_list_child">
+                                    <li><a href="">CN phần mềm</a></li>
+                                    <li><a href="">Hệ thống thông tin</a></li>
+                                    <li><a href="">Toán học</a></li>
+                                    <li><a href="">Tin học và kĩ thuật tính toán</a></li>
+                                    <li><a href="">Trí tuệ nhân tạo</a></li>
+                                    <li><a href="">Mạng và an toàn thông tin</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="">Tuyển sinh</a>
+                                <ul class="menu_list_child">
+                                    <li><a href="">Nhóm ngành CNTT</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="">Sinh viên</a>
+                                <ul class="menu_list_child">
+                                    <li><a href="">Tài liệu sinh viên</a></li>
+                                    <li><a href="">Tổ tư vấn học tập</a></li>
+                                    <li><a href="">Biểu mẫu TADN</a></li>
+                                    <li><a href="">Luận văn tốt nghiệp</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="">Tin tức</a>
+                                <ul class="menu_list_child">
+                                    <li><a href="">Sự kiện</a></li>
+                                    <li><a href=""></a>CSE trên báo</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="">Thông báo</a>
+                                <ul class="menu_list_child">
+                                    <li><a href="">Thông báo</a></li>
+                                    <li><a href="">TB đào đạo</a></li>
+                                    <li><a href="">Nghiên cứu khoa học</a></li>
+                                    <li><a href="">Tuyển dụng</a></li>
+                                    <li><a href="">Học bổng</a></li>
+                                    <li><a href=""></a>Thông báo khác</li>
+                                </ul>
+                            </li>
+                            <li id=" menu_list_contac">
+                                <a href="">Liên hệ</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="wrapper">
+                <div class="container" style="margin: 0 482px;">
+                    <div class="row">
+                        <div class="col-7">
+                            <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+                                <div class="carousel-indicators">
+                                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                </div>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active" data-bs-interval="10000">
+                                        <img src="/assets/images/img/slideshow_1.jpg" style="height: 286px;" class="d-block w-100" alt="...">
+                                    </div>
+                                    <div class="carousel-item" data-bs-interval="2000">
+                                        <img src="/assets/images/img/slideshow_2.jpg" style="height: 286px;" class="d-block w-100" alt="...">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="./assets/images/img/slideshow_3.jpg" style="height: 286px;" class="d-block w-100" alt="...">
+                                    </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
+                            
+                        </div>
+                        <div class="main">
+                            <h1> Tuyển Sinh</h1>
+                                
+                                 <div class="row">
+                                    <div class="col-3 one"> <img alt="" class="left"
+                                    src="img/tuyensinh1.png"></div>
+                                    <div class="col-9 tow" >
+                                    <h2>
+                                        Thông tin tuyển sinh cho đại học chính quy 2022                
+                                    </h2> </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-3 one"> <img alt="" class="left"
+                                    src="img/tuyensinh1.png"></div>
+                                    <div class="col-9 tow" >
+                                    <h2>
+                                        Thông tin tuyển sinh cho đại học chính quy 2022                
+                                    </h2> </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-3 one"> <img alt="" class="left"
+                                    src="img/tuyensinh1.png"></div>
+                                    <div class="col-9 tow" >
+                                    <h2>
+                                        Thông tin tuyển sinh cho đại học chính quy 2022                
+                                    </h2> </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-3 one"> <img alt="" class="left"
+                                    src="img/tuyensinh1.png"></div>
+                                    <div class="col-9 tow" >
+                                    <h2>
+                                        Thông tin tuyển sinh cho đại học chính quy 2022                
+                                    </h2> </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-3 one"> <img alt="" class="left"
+                                    src="img/tuyensinh1.png"></div>
+                                    <div class="col-9 tow" >
+                                    <h2>
+                                        Thông tin tuyển sinh cho đại học chính quy 2022                
+                                    </h2> </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-3 one"> <img alt="" class="left"
+                                    src="img/tuyensinh1.png"></div>
+                                    <div class="col-9 tow" >
+                                    <h2>
+                                        Thông tin tuyển sinh cho đại học chính quy 2022                
+                                    </h2> </div>
+                                </div>
+
+                        </div>    
+                    </div>
+                </div>
+
+        </div>
+
+        <div class="foodter">
+            <div class="container" style="margin: 0 400px;">
+                <div class="foodter_top">
+                    <div class="foodter_menu">
+                        <ul class="foodter_list">
+                            <li><a href="">Giới thiệu</a></li>
+                            <li><a href="">Logo khoa CNTT</a></li>
+                            <li><a href="">Lời chào mừng</a></li>
+                            <li><a href="">Tổ chức</a></li>
+                            <li><a href="">Hợp tác liên kết</a></li>
+                        </ul>
+                    </div>
+                    <div class="foodter_menu">
+                        <ul class="foodter_list">
+                            <li><a href="">Đào tạo</a></li>
+                            <li><a href="">Mô hình đào tạo</a></li>
+                            <li><a href="">Đào tạo đại học</a></li>
+                            <li><a href="">Định hướng ngành nghề</a></li>
+                            <li><a href="">Đào tạo sau đại học</a></li>
+                        </ul>
+                    </div>
+                    <div class="foodter_menu">
+                        <ul class="foodter_list">
+                            <li><a href="">Nghiên cứu khoa học</a></li>
+                            <li><a href="">Thông tin semiar</a></li>
+                            <li><a href="">Các đề tài dự án</a></li>
+                            <li><a href="">Công trình công bố</a></li>
+                            <li><a href="">Các phòng thí nghiệm</a></li>
+                        </ul>
+                    </div>
+                    <div class="foodter_menu">
+                        <ul class="foodter_list">
+                            <li><a href="">Công nghệ phần mềm</a></li>
+                            <li><a href="">Hệ thống thông tin</a></li>
+                            <li><a href="">Khoa học máy tính</a></li>
+                            <li><a href="">Kĩ thuật máy tính và mạng</a></li>
+                            <li><a href="">Toán học</a></li>
+                            <li><a href="">Trung tâm tin học</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="foodter_buttom">
+                    <img src="/assets/images/img/foodter_1.png" alt="">
+                    <p>© 2017 Khoa Công nghệ thông tin - Đại học Thủy lợi<br>
+                        Địa chỉ: nhà C1, Đại học Thủy lợi, 175 Tây Sơn, Đống Đa, Hà Nội. Điện thoại: (+84)-024&nbsp;3 5632211. Email:<a class="ui-link-white" href="#"> vpkcntt@tlu.edu.vn</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
